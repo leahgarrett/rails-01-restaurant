18a0f49d48eafd253e94e6c97bb6c37976c6dbec
Leah Garrett <leah_garrett@hotmail.com>
2019-04-17 14:45:42 +1000

    starting on reviews

--- db/migrate/20190417041445_create_restaurants.rb
+++ db/migrate/20190417041445_create_restaurants.rb
@@ -5,6 +5,7 @@ class CreateRestaurants < ActiveRecord::Migration[5.2]
       t.string :address
       t.string :description
       t.string :food_type
+      t.belongs_to :restaurant, index: true
 
       t.timestamps
     end
